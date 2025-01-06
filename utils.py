98e21781a148a2194be15a40a61aed72296790cc
Carlos Natalino <6130746+carlosnatalino@users.noreply.github.com>
2025-01-06 22:20:37 +0100

    Updates for 2025.

--- utils.py
+++ utils.py
@@ -2,89 +2,33 @@
 # file to be used in the context of the EEN060/EEN065 course at Chalmers ONLY
 #  THIS FILE MUST NOT BE CHANGED BY STUDENTS
 
+import getpass
 import json
 import os
 from datetime import datetime
-from typing import Any, Dict
+from typing import Any
 
 import requests
-from bs4 import BeautifulSoup
-from IPython.display import display, HTML  # type: ignore
+from IPython.display import display, HTML
 
-url_html_validator = "https://onu1.s2.chalmers.se/nu/?out=json"
-url_html_validator_fallback = "https://validator.w3.org/nu/?out=json"
 url_python_validator = "https://onu1.s2.chalmers.se/pnu/"
-# url_python_validator = "http://localhost:5000/"
 
 
-def validate_html(html: str) -> BeautifulSoup:
-    response_html = None
-    try:
-        response_html = requests.post(
-            url_html_validator,
-            headers={"Content-Type": "text/html; charset=UTF-8"},
-            data=html,
-            timeout=30,
-        )
-        print("first")
-    except Exception:
-        response_html = requests.post(
-            url_html_validator_fallback,
-            headers={"Content-Type": "text/html; charset=UTF-8"},
-            data=html,
-            timeout=30,
-        )
-    message = ""
-    has_error = False
-    if response_html.json()["messages"]:
-        has_error = True
-        for key, value in response_html.json().items():
-            if not isinstance(value, list):
-                message += key + " |-> " + value + "\n"
-            else:
-                error_number = 1
-                html_split = html.split("\n")
-                for i in value:
-                    message += "\n"
-                    if "type" in i:
-                        if i["type"] == "error":
-                            message += f"\tError {error_number}:" + "\n"
-                        elif i["type"] == "warning":
-                            message += f"\tWarning {error_number}:" + "\n"
-                        else:
-                            message += f"""\t{i["type"]} {error_number}:\n"""
-                        if error_number == 1:
-                            message += (
-                                "\t\tThis is probably the one to look for first!" + "\n"
-                            )
-                        message += "\t\tMessage: " + i["message"] + "\n"
-                        initial_line = max(0, i["lastLine"] - 3)
-                        end_line = min(len(html_split) - 1, i["lastLine"] + 2)
-                        message += f"""\t\tLine with problem: {i["lastLine"] - 1}\n"""
-                        message += "\t\tCheck the code below:\n"
-                        for j in range(initial_line, end_line):
-                            mark = ""
-                            if j + 1 == i["lastLine"]:
-                                mark = ">>"
-                            message += f"""\t\t{j}: {mark}\t{html_split[j]}\n"""
-                        error_number += 1
-                    else:
-                        for k2, v2 in i.items():
-                            message += "\t" + str(k2) + " -> " + str(v2) + "\n\n"
-    if has_error:
-        raise ValueError(f"HTML error:\n{message}")
-    soup = BeautifulSoup(html, "html.parser")
-    return soup
-
-
-def validate_python_code(filename: str, **args: Dict[str, Any]) -> None:  # type: ignore
+def validate_python_code(filename: str, **args: dict[str, Any]) -> None:  # type: ignore
+    if "blank_space" not in args:
+        args["blank_space"] = "&middot;"  # type: ignore
+    if "line_index_offset" not in args:
+        args["line_index_offset"] = 1  # type: ignore
+    if "format" not in args:
+        args["format"] = "notes"  # type: ignore
     if not os.path.exists(filename):
         raise ValueError(f"The file {filename} does not exist. Run the solution cell!")
 
-    payload: Dict[str, Any] = dict()  # type: ignore
+    payload: dict[str, Any] = dict()  # type: ignore
     if args:
         payload = args.copy()
     payload["date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S %z")
+    payload["user"] = getpass.getuser()
 
     try:  # reading the file
         with open(filename, "rt", encoding="UTF-8") as file_obj:
@@ -128,6 +72,7 @@ def validate_python_code(filename: str, **args: Dict[str, Any]) -> None:  # type
         ) from e
     try:
         data = response_html.json()
+        # print(data["result_analysis"])
         display(HTML(data["result_analysis"]))  # type: ignore
     except Exception as e:
         # pylint: disable=raise-missing-from
--- validate_installation.py
+++ validate_installation.py
@@ -1,6 +1,10 @@
 # these imports help us validate that the installation was correct
+import getpass
+import datetime
+import os
 import time
 import random
+
 import matplotlib.pyplot as plt
 
 numbers = []
@@ -11,7 +15,13 @@ for i in range(100):
         numbers.append(random.randint(10, 20))
 
 plt.figure()
-plt.title("If you see this, your installation was successful!")
+plt.title(
+    f"""If you see this, your installation was successful!
+    Date: {datetime.datetime.now()}
+    Folder: {os.getcwd()}
+    User: {getpass.getuser()}"""
+)
 plt.plot(numbers)
+plt.tight_layout()
 plt.show()
 plt.close()
